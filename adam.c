a281b54e83cf79765ecc3f0798b1b16e0c172a45
Isaac Volk <isaacvolk3@gmail.com>
2021-03-20 01:08:59 -0400

    Made early loadModel function

--- adam.c
+++ adam.c
@@ -30,23 +30,32 @@ int checkEvents(SDL_Event event)
     return 1;
 }
 
+void loadModel(unsigned int *aVAO, unsigned int *aVBO, float *data, int size)
+{
+    // Generate arrays and buffers
+    glGenVertexArrays(1, aVAO);
+    glGenBuffers(1, aVBO);
+    glBindVertexArray(*aVAO);
+    // Copy vertices into buffer
+    glBindBuffer(GL_ARRAY_BUFFER, *aVBO);
+    glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+    // Set buffer attributes or how the data is organized to array
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Turn both buffer and array off
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
 int main(int argc, char *args[])
 {
     initSystems();
     SDL_Window *window = setWindow();
 
     unsigned int shaderProgram = setupShaderPrgm();
-    unsigned int VBO, VAO;
+    unsigned int *VBO, *VAO;
 
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
-    // Copy vertices into buffer
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-    // Set buffer attributes or how the data is organized
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    loadModel(VAO, VBO, verts, sizeof(verts));
 
     int quit = 1;
     SDL_Event event;
@@ -64,7 +73,7 @@ int main(int argc, char *args[])
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        glBindVertexArray(*VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         SDL_GL_SwapWindow(window);
     }
